d56210ccb93b89f37000967562e9c053c3d32858
Mylloon <kennel.anri@tutanota.com>
2022-08-20 19:44:56 +0200

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::Parser;
+
+mod parse;
+
+#[derive(Parser)]
+#[clap(version, about, long_about = None)]
+struct Cli {
+    /// Your username
+    #[clap(value_parser)]
+    username: String,
+
+    /// Directory output [default: the username]
+    #[clap(short, long, value_parser, value_name = "DIRECTORY")]
+    directory: Option<String>,
+
+    /// Domain name
+    #[clap(
+        long,
+        value_parser,
+        value_name = "DOMAIN NAME",
+        default_value = "prose.sh"
+    )]
+    domain: String,
+
+    /// Scheme: HTTP/HTTPS
+    #[clap(long, value_parser, default_value = "https")]
+    scheme: String,
+}
+
 fn main() {
-    println!("Hello, prose!");
+    let cli = Cli::parse();
+
+    let posts = parse::get_posts(
+        cli.scheme.to_lowercase(),
+        cli.username.to_lowercase(),
+        cli.domain.to_lowercase(),
+    );
+
+    println!("{:#?}", posts);
 }
